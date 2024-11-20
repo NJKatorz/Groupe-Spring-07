2d78692c09a3220f7472d376ee173c94377ed659
NJKatorz <nathanesakwa@gmail.com>
2024-11-20 16:04:51 +0100

    add: methods for UsersProxy and TargetsProxy classes

--- gateway/src/main/java/be/vinci/ipl/projet2024/group07/gateway/GatewayService.java
+++ gateway/src/main/java/be/vinci/ipl/projet2024/group07/gateway/GatewayService.java
@@ -7,11 +7,16 @@ import be.vinci.ipl.projet2024.group07.gateway.data.ServersProxy;
 import be.vinci.ipl.projet2024.group07.gateway.data.TargetsProxy;
 import be.vinci.ipl.projet2024.group07.gateway.data.UsersProxy;
 import be.vinci.ipl.projet2024.group07.gateway.exceptions.BadRequestException;
+import be.vinci.ipl.projet2024.group07.gateway.exceptions.ConflictException;
 import be.vinci.ipl.projet2024.group07.gateway.exceptions.ForbiddenException;
+import be.vinci.ipl.projet2024.group07.gateway.exceptions.NotFoundException;
 import be.vinci.ipl.projet2024.group07.gateway.exceptions.UnauthorizedException;
 import be.vinci.ipl.projet2024.group07.gateway.models.Credentials;
+import be.vinci.ipl.projet2024.group07.gateway.models.Target;
 import feign.FeignException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class GatewayService {
@@ -23,7 +28,9 @@ public class GatewayService {
   private final TargetsProxy targetsProxy;
   private final UsersProxy usersProxy;
 
-  public GatewayService(AttacksProxy attacksProxy, AuthenticationProxy authenticationProxy, ExploitsProxy exploitsProxy, ServersProxy serversProxy, TargetsProxy targetsProxy, UsersProxy usersProxy) {
+  public GatewayService(AttacksProxy attacksProxy, AuthenticationProxy authenticationProxy,
+      ExploitsProxy exploitsProxy, ServersProxy serversProxy, TargetsProxy targetsProxy,
+      UsersProxy usersProxy) {
     this.attacksProxy = attacksProxy;
     this.authenticationProxy = authenticationProxy;
     this.exploitsProxy = exploitsProxy;
@@ -46,7 +53,8 @@ public class GatewayService {
     }
   }
 
-  public void verify(String token, String expectedUserEmail) throws ForbiddenException, UnauthorizedException {
+  public void verify(String token, String expectedUserEmail)
+      throws ForbiddenException, UnauthorizedException {
     try {
       String userEmail = authenticationProxy.verify(token);
       if (!userEmail.equals(expectedUserEmail)) {
@@ -61,4 +69,62 @@ public class GatewayService {
     }
   }
 
+
+  public Iterable<Target> readAllTargets() {
+    return targetsProxy.readAll();
+  }
+
+  public void createTarget(Target target) throws BadRequestException, ConflictException {
+    try {
+      targetsProxy.createOne(target);
+    } catch (FeignException e) {
+      if (e.status() == 400) {
+        throw new BadRequestException();
+      } else if (e.status() == 409) {
+        throw new ConflictException();
+      } else {
+        throw e;
+      }
+    }
+  }
+
+  public Target readTarget(int targetId) throws NotFoundException {
+    try {
+      return targetsProxy.readOne(targetId);
+    } catch (FeignException e) {
+      if (e.status() == 404) {
+        throw new NotFoundException();
+      } else {
+        throw e;
+      }
+    }
+  }
+
+  public void updateTarget(int targetId, Target target) throws BadRequestException, NotFoundException {
+    try {
+      targetsProxy.updateOne(targetId, target);
+    } catch (FeignException e) {
+      if (e.status() == 400) {
+        throw new BadRequestException();
+      } else if (e.status() == 404) {
+        throw new NotFoundException();
+      } else {
+        throw e;
+      }
+    }
+  }
+
+  public void deleteTarget(int targetId) throws NotFoundException {
+    try {
+      targetsProxy.deleteOne(targetId);
+    } catch (FeignException e) {
+      if (e.status() == 404) throw new NotFoundException();
+      else throw e;
+    }
+  }
+
+  public Iterable<Target> readColocated(){
+    return targetsProxy.readAll();
+  }
 }
+
--- gateway/src/main/java/be/vinci/ipl/projet2024/group07/gateway/data/TargetsProxy.java
+++ gateway/src/main/java/be/vinci/ipl/projet2024/group07/gateway/data/TargetsProxy.java
@@ -1,10 +1,34 @@
 package be.vinci.ipl.projet2024.group07.gateway.data;
 
+import be.vinci.ipl.projet2024.group07.gateway.models.Target;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Repository
 @FeignClient(name = "targets")
 public interface TargetsProxy {
 
+  @GetMapping("/targets")
+  Iterable<Target> readAll();
+
+  @PostMapping("/targets")
+  void createOne(@RequestBody Target target);
+
+  @GetMapping("/targets/{targetId}")
+  Target readOne(@PathVariable int targetId);
+
+  @PutMapping("/targets/{targetId}")
+  void updateOne(@PathVariable int targetId, @RequestBody Target target);
+
+  @DeleteMapping("/targets/{targetId}")
+  void deleteOne(@PathVariable int targetId);
+
+  @GetMapping("/targets/colocated")
+  Iterable<Target> readColocated();
 }
