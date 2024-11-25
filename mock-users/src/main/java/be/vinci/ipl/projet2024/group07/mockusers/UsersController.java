ac6a45efdcd8c5441043674cd25815e9404ad259
steven-eklou-vinci <steven.eklou@student.vinci.be>
2024-11-25 22:03:56 +0100

    refactor: refactoring of the mock-user and mock-exploit

--- mock-users/src/main/java/be/vinci/ipl/projet2024/group07/mockusers/UsersController.java
+++ mock-users/src/main/java/be/vinci/ipl/projet2024/group07/mockusers/UsersController.java
@@ -8,36 +8,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
 public class UsersController {
-  @PostMapping("/{username}")
-  public ResponseEntity<Void> createUser(@PathVariable String username, @RequestBody UserWithCredentials user) {
-    if (user.invalid() || !user.getUsername().equals(username)) {
-      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    }
+  @PostMapping("/users/{pseudo}")
+  public ResponseEntity<Void> createOne(@PathVariable String pseudo, @RequestBody UserWithCredentials user) {
     return new ResponseEntity<>(HttpStatus.CREATED);
   }
 
-  @GetMapping("/{username}")
-  public ResponseEntity<User> getUser(@PathVariable String username) {
-    User user = new User(username, "John", "Doe");
-    return ResponseEntity.ok(user);
+  @GetMapping("/users/{pseudo}")
+  public User readOne(@PathVariable String pseudo) {
+    User user = new User();
+    user.setPseudo(pseudo);
+    user.setFirstname("test");
+    user.setLastname("test");
+    return user;
   }
 
-  @PutMapping("/{username}")
-  public ResponseEntity<Void> updateUser(@PathVariable String username, @RequestBody UserWithCredentials user) {
-    if (user.invalid() || !user.getUsername().equals(username)) {
-      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    }
-    return new ResponseEntity<>(HttpStatus.OK);
+  @PutMapping("/users/{pseudo}")
+  public void updateOne(@PathVariable String pseudo, @RequestBody UserWithCredentials user) {
   }
 
-  @DeleteMapping("/{username}")
-  public ResponseEntity<Void> deleteUser(@PathVariable String username) {
-    return new ResponseEntity<>(HttpStatus.OK);
+  @DeleteMapping("/users/{pseudo}")
+  public void deleteOne(@PathVariable String pseudo) {
   }
 }
