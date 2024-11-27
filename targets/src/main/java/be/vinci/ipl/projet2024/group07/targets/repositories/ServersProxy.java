0526dafce22fbe732b4ad666185065ad81834714
Mamadou Barry <mamadou.barry@student.vinci.be>
2024-11-27 15:19:41 +0100

    Add: end off Target. Still need to test it

--- targets/src/main/java/be/vinci/ipl/projet2024/group07/targets/repositories/ServersProxy.java
+++ targets/src/main/java/be/vinci/ipl/projet2024/group07/targets/repositories/ServersProxy.java
@@ -4,12 +4,13 @@ import be.vinci.ipl.projet2024.group07.targets.models.Server;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Repository
 @FeignClient(name = "users")
 public interface ServersProxy {
 
-  @GetMapping("/servers")
-  Server readOne();
+  @GetMapping("/servers/target/{targetId}")
+  Iterable<Server> readByTarget(@PathVariable int targetId);
 
 }
