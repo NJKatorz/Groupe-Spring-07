b7fe51e13c6e5c6edf5c213eb27366bc0795e839
steven-eklou-vinci <steven.eklou@student.vinci.be>
2024-11-27 17:53:06 +0100

    refactor: refactoring of the UserRepo interface

--- users/src/main/java/be/vinci/ipl/projet2024/group07/users/repositories/UsersRepository.java
+++ users/src/main/java/be/vinci/ipl/projet2024/group07/users/repositories/UsersRepository.java
@@ -8,6 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UsersRepository extends CrudRepository<User, Integer> {
   boolean existsByEmail(String email);
-  User findByEmail(String email);
 
 }
