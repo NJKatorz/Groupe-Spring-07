0a0af6350bd75b504ba28aca140009d1b9c32473
steven-eklou-vinci <steven.eklou@student.vinci.be>
2024-11-29 03:21:03 +0100

    refactor: refactoring of user module

--- users/src/main/java/be/vinci/ipl/projet2024/group07/users/repositories/UsersRepository.java
+++ users/src/main/java/be/vinci/ipl/projet2024/group07/users/repositories/UsersRepository.java
@@ -1,6 +1,7 @@
 package be.vinci.ipl.projet2024.group07.users.repositories;
 
 import be.vinci.ipl.projet2024.group07.users.models.User;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UsersRepository extends CrudRepository<User, Integer> {
   boolean existsByEmail(String email);
+  Optional<User> findByEmail(String email);
+
+  Optional<User> findById (int id);
 
 }
